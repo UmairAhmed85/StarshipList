ac17fd8e5049748e6cc6b63c2207b66314c5bee5
Umair <uahmed@eddress.com>
2022-11-28 01:02:48 +0500

    Adding logger to api request

--- app/src/main/java/com/example/glacticjourney/domain/Repository.kt
+++ app/src/main/java/com/example/glacticjourney/domain/Repository.kt
@@ -5,17 +5,36 @@ import com.example.glacticjourney.data.dto.PilotDto
 import com.example.glacticjourney.data.dto.Result
 import com.example.glacticjourney.data.remote.GlacticApi
 import kotlinx.coroutines.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class Repository {
     private val TAG = "Repository"
     companion object {
+
+       val okHttpClient =  OkHttpClient.Builder()
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .addInterceptor { chain ->
+            val original = chain.request()
+            val requestBuilder = original.newBuilder()
+                .method(original.method, original.body)
+//                .addHeader("Authorization", "Bearer " + Utilities.jwtToken)
+            val request = requestBuilder.build()
+            chain.proceed(request)
+        }.build()
+
         val api = Retrofit
             .Builder()
             .baseUrl(GlacticApi.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(GlacticApi::class.java)
 
